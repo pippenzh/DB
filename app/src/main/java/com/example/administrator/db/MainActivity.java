95cc3d0ef89a712860fd090a6275cba29234eac4
lsx <saronic@yeah.net>
2015-07-28 09:01:03 +0800

    可以写入数据库

---
+++ app/src/main/java/com/example/administrator/db/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.administrator.db;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyDBHelper myDBHelper = new MyDBHelper(this, "manage", 1);
+        SQLiteDatabase dbWriter = myDBHelper.getWritableDatabase();
+        ContentValues cv  = new ContentValues();
+        cv.put("name", "zhangsan");
+        cv.put("phone", "12345");
+        dbWriter.insert("user", null, cv);
+
+        cv  = new ContentValues();
+        cv.put("name", "lisi");
+        cv.put("phone", "7890");
+        dbWriter.insert("user",null,  cv);
+
+        dbWriter.close();
+
+    }
+
+}
