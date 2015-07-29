e32546822e491c1d845e54d5b2c0b6ee5e7befc9
lsx <saronic@yeah.net>
2015-07-29 08:58:08 +0800

    显示内容到ListView

--- app/src/main/java/com/example/administrator/db/MainActivity.java
+++ app/src/main/java/com/example/administrator/db/MainActivity.java
@@ -9,14 +9,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private ListView mListView;
+    private SimpleCursorAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.lv_user);
+
         MyDBHelper myDBHelper = new MyDBHelper(this, "manage", 4);
 //        SQLiteDatabase dbWriter = myDBHelper.getWritableDatabase();
 //        ContentValues cv  = new ContentValues();
@@ -31,9 +36,12 @@ public class MainActivity extends ActionBarActivity {
 //
 //        dbWriter.close();
 
-        SQLiteDatabase dbReader =  myDBHelper.getReadableDatabase();
-        Cursor c = dbReader.query("user", null, "name=? and phone=?", new String[]{"lisi", "7890"}, null, null, null);
+        SQLiteDatabase dbReader = myDBHelper.getReadableDatabase();
+        //Cursor c = dbReader.query("user", null, "name=? and phone=?", new String[]{"lisi", "7890"}, null, null, null);
+        Cursor c = dbReader.query("user", null, null, null, null, null, null );
 
+        mAdapter = new SimpleCursorAdapter(this, R.layout.list_item, c, new String[]{"name"}, new int[] {R.id.tv_name});
+        mListView.setAdapter(mAdapter);
         while (c.moveToNext()) {
             String name = c.getString(c.getColumnIndex("name"));
             String phone = c.getString(c.getColumnIndex("phone"));
